a4afc8dbc63c7f3429d76eb33cce614f2acb1fb0
Klein Lieu <kleinvinhlieu@gmail.com>
2017-01-12 16:13:02 -0800

    Adding a suggestion to use static IDs

--- rocketStats/ViewController.swift
+++ rocketStats/ViewController.swift
@@ -23,6 +23,19 @@ class ViewController: UIViewController {
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Usually people would want to declare their string staticly
+        // at the top of the class file like so
+        // ```
+        // static let kVCSegueIDXbox = "toXbox"
+        // static let kVCSegueIDPSN = "toPsn"
+        // ...
+        // if segue.identifier == kVCSegueIDXbox  {}
+        // ```
+        // Note: k stands for constant btw.
+        // This is both an optimization (since a string literal does not get allocated multiple times)
+        // and a concise coding style. That way if you ever have to reference your segue IDs,
+        // you only have to change them in one place. 
+
         if segue.identifier == "toXbox"{
             let vc = segue.destination as! xboxOne
             vc.xboxTag = gamerTag.text!
